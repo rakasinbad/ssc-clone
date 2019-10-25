635bc7b61cab9bddeb3c59287feb4a59a26c1e5e
Rachmad Setiawan <iwan@sinbad.co.id>
2019-10-25 18:25:45 +0700

    Menambah komponen: - catalogues-add-new-product - catalogues-form - catalogues-import

---
+++ src/app/main/pages/catalogues/catalogues-import/catalogues-import.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogues-import',
+  templateUrl: './catalogues-import.component.html',
+  styleUrls: ['./catalogues-import.component.scss']
+})
+export class CataloguesImportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
