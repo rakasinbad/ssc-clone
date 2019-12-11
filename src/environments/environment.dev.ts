1b86c66c900587d870f7fb35b9b7d51001f73b4f
Rachmad Setiawan <iwan@sinbad.co.id>
2019-12-11 22:04:16 +0700

    Add environment file for development stage

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    pageSize: 100,
+    production: true,
+    staging: false,
+    hmr: false,
+    freshnessData: 60, // in seconds.
+    host: 'https://kong-dev.sinbad.web.id'
+};
