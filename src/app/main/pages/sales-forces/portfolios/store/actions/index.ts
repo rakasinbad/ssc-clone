73fa0d2538f70110e682847921f90fb359853c1b
Rachmad Setiawan <iwan@sinbad.co.id>
2019-12-27 16:11:00 +0700

    Add store.actions to index actions

--- src/app/main/pages/sales-forces/portfolios/store/actions/index.ts
+++ src/app/main/pages/sales-forces/portfolios/store/actions/index.ts
@@ -1,10 +1,12 @@
 import * as PortfolioActions from './portfolios.actions';
+import * as StoreActions from './stores.actions';
 
-type portfolioRequestActionNames = PortfolioActions.requestActionNames;
-type portfolioFailureActionNames = PortfolioActions.failureActionNames;
+type portfolioRequestActionNames = PortfolioActions.requestActionNames | StoreActions.requestActionNames;
+type portfolioFailureActionNames = PortfolioActions.failureActionNames | StoreActions.failureActionNames;
 
 export {
     PortfolioActions,
+    StoreActions,
     portfolioRequestActionNames,
     portfolioFailureActionNames,
 };
