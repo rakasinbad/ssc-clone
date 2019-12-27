5353ae96a55708d7a3efdbf48fdd4d996ba9ded3
Rachmad Setiawan <iwan@sinbad.co.id>
2019-12-27 16:09:47 +0700

    Add function for find portfolio stores and create portfolios

--- src/app/main/pages/sales-forces/portfolios/services/portfolios-api.service.ts
+++ src/app/main/pages/sales-forces/portfolios/services/portfolios-api.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HelperService } from 'app/shared/helpers';
 import { HttpClient } from '@angular/common/http';
-import { Portfolio } from '../models/portfolios.model';
+import { Portfolio, IPortfolioAddForm } from '../models/portfolios.model';
 import { IQueryParams, IPaginatedResponse } from 'app/shared/models';
 import { Observable } from 'rxjs';
+import { Store } from 'app/main/pages/attendances/models';
 
 @Injectable({
     providedIn: 'root'
@@ -13,6 +14,7 @@ export class PortfoliosApiService {
     private _url: string;
 
     private readonly _endpoint = '/portfolios';
+    private readonly _storeEndpoint = '/stores';
     private readonly _exportEndpoint = '/download/export-portfolios';
 
     constructor(
@@ -44,5 +46,26 @@ export class PortfoliosApiService {
         const newParams = this.helper$.handleParams(this._url, params, ...newArgs);
 
         return this.http.get<IPaginatedResponse<Portfolio>>(this._url, { params: newParams });
+    }
+
+    findPortfolioStores(params: IQueryParams): Observable<IPaginatedResponse<Store>> {
+        const newArgs = [];
+
+        if (!isNaN(params['portfolioId'])) {
+            newArgs.push({
+                key: 'portfolioId',
+                value: params['portfolioId']
+            });
+        }
+
+        this._url = this.helper$.handleApiRouter(this._storeEndpoint);
+        const newParams = this.helper$.handleParams(this._url, params, ...newArgs);
+
+        return this.http.get<IPaginatedResponse<Store>>(this._url, { params: newParams });
+    }
+
+    createPortfolio(data: IPortfolioAddForm): Observable<Portfolio> {
+        this._url = this.helper$.handleApiRouter(this._endpoint);
+        return this.http.post<Portfolio>(this._url, data);
     }
 }
