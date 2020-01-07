ed570943e56d2ee81f008aaf645774c6731361bf
Rachmad Setiawan <iwan@sinbad.co.id>
2020-01-07 14:56:27 +0700

    Add i18n for associations module

---
+++ src/app/main/pages/sales-forces/associations/i18n/id.ts
@@ -0,0 +1,15 @@
+import { globalIdLang } from 'app/lang/i18n/id';
+
+export const locale = {
+    lang: 'id',
+    data: {
+        ...globalIdLang,
+        BREADCRUMBS: {
+            ...globalIdLang.BREADCRUMBS,
+            ASSOCIATION: 'Association',
+            ASSOCIATION_ADD: 'Add Association',
+            ASSOCIATION_EDIT: 'Edit Association',
+            ASSOCIATION_DETAIL: 'Detail Association'
+        }
+    }
+};
