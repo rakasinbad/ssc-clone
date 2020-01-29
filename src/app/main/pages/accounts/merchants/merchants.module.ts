c686537001aa98fc1d6b998f3a65ea5a557db95d
Aulia Rahman <aulia@sinbad.co.id>
2020-01-29 17:52:07 +0700

    - Change icon, fix size column at store list

--- src/app/main/pages/accounts/merchants/merchants.module.ts
+++ src/app/main/pages/accounts/merchants/merchants.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { RxReactiveDynamicFormsModule } from '@rxweb/reactive-dynamic-forms';
 import { RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
+import { ImportAdvancedModule } from 'app/shared';
 import { MaterialModule } from 'app/shared/material.module';
 import { SharedModule } from 'app/shared/shared.module';
 import { NgxPermissionsModule } from 'ngx-permissions';
@@ -42,6 +42,8 @@ import { fromMerchant } from './store/reducers';
 
         SharedModule,
         MaterialModule,
+
+        ImportAdvancedModule,
 
         // AgmCoreModule,
         // LeafletModule,
