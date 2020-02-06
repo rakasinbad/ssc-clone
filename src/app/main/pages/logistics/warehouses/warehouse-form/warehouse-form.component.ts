03527d499cf7942b68dcaa5fd3505385ea2514ff
Aulia Rahman <aulia@sinbad.co.id>
2020-02-06 17:19:19 +0700

    - Progress layout warehouse list

--- src/app/main/pages/logistics/warehouses/warehouse-form/warehouse-form.component.ts
+++ src/app/main/pages/logistics/warehouses/warehouse-form/warehouse-form.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { fuseAnimations } from '@fuse/animations';
 
 @Component({
     selector: 'app-warehouse-form',
     templateUrl: './warehouse-form.component.html',
     styleUrls: ['./warehouse-form.component.scss'],
+    animations: fuseAnimations,
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
