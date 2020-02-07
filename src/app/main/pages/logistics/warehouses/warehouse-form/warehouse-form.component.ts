52ec87937a99496941201fb908ae590636877fda
Aulia Rahman <aulia@sinbad.co.id>
2020-02-07 13:16:52 +0700

    - Layouting warehouse form

--- src/app/main/pages/logistics/warehouses/warehouse-form/warehouse-form.component.ts
+++ src/app/main/pages/logistics/warehouses/warehouse-form/warehouse-form.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { fuseAnimations } from '@fuse/animations';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     selector: 'app-warehouse-form',
@@ -10,10 +11,37 @@ import { fuseAnimations } from '@fuse/animations';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WarehouseFormComponent implements OnInit {
-    constructor() {}
+    pageType: string;
+
+    opts = {
+        minZoom: 3,
+        maxZoom: 18,
+        zoom: 5,
+        lat: -2.5,
+        lng: 117.86,
+        icon: {
+            url: 'assets/images/marker.png',
+            scaledSize: {
+                width: 18,
+                height: 30
+            }
+        }
+    };
+
+    constructor(private route: ActivatedRoute, private router: Router) {}
 
     ngOnInit(): void {
         // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
         // Add 'implements OnInit' to the class.
+
+        const { id } = this.route.snapshot.params;
+
+        if (id === 'new') {
+            this.pageType = 'new';
+        } else if (Math.sign(id) === 1) {
+            this.pageType = 'edit';
+        } else {
+            this.router.navigateByUrl('/pages/logistics/warehouses');
+        }
     }
 }
