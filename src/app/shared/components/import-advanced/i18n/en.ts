d3bce5841dad3754b9544801a349514d8b88abe7
Rachmad Setiawan <iwan@sinbad.co.id>
2020-02-07 14:19:32 +0700

    Added translation for import advanced dialog

--- src/app/shared/components/import-advanced/i18n/en.ts
+++ src/app/shared/components/import-advanced/i18n/en.ts
@@ -5,6 +5,8 @@ export const locale = {
     data: {
         ...globalEnLang,
         STATUS: 'Status',
+        CREATE_STORE: 'Create Store',
+        CREATE_STORES: 'Create Stores',
         UPDATE_PAYMENT_STATUS: 'Update Payment Status',
         UPDATE_STATUS_OMS: 'Update Status OMS',
         UPDATE: 'Update'
--- src/app/shared/components/import-advanced/i18n/id.ts
+++ src/app/shared/components/import-advanced/i18n/id.ts
@@ -5,6 +5,8 @@ export const locale = {
     data: {
         ...globalIdLang,
         STATUS: 'Status',
+        CREATE_STORE: 'Create Store',
+        CREATE_STORES: 'Create Stores',
         UPDATE_PAYMENT_STATUS: 'Update Payment Status',
         UPDATE_STATUS_OMS: 'Update Status OMS',
         UPDATE: 'Update'
