68d3875c365ec5d4edfa618e77af2e306efc6df4
Rachmad Setiawan <iwan@sinbad.co.id>
2020-02-24 17:01:56 +0700

    Added import functionality in Portfolio's page

--- src/app/main/pages/sales-forces/portfolios/i18n/en.ts
+++ src/app/main/pages/sales-forces/portfolios/i18n/en.ts
@@ -4,6 +4,10 @@ export const locale = {
     lang: 'en',
     data: {
         ...globalEnLang,
+        IMPORT_PRICE: 'Import Price',
+        UPDATE_PRICE_TEMPLATE: 'Update Price',
+        IMPORT_STOCK: 'Import Stock',
+        UPDATE_STOCK_TEMPLATE: 'Update Stock',
         BREADCRUMBS: {
             ...globalEnLang.BREADCRUMBS,
             PORTFOLIO: 'Portfolio',
--- src/app/main/pages/sales-forces/portfolios/i18n/id.ts
+++ src/app/main/pages/sales-forces/portfolios/i18n/id.ts
@@ -4,6 +4,10 @@ export const locale = {
     lang: 'id',
     data: {
         ...globalIdLang,
+        IMPORT_PRICE: 'Import Price',
+        UPDATE_PRICE_TEMPLATE: 'Update Price',
+        IMPORT_STOCK: 'Import Stock',
+        UPDATE_STOCK_TEMPLATE: 'Update Stock',
         BREADCRUMBS: {
             ...globalIdLang.BREADCRUMBS,
             PORTFOLIO: 'Portfolio',
