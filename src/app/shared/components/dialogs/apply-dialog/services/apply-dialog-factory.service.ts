9fee2f23f8c768f8a3b6b8fb34e4c4df651afca0
Rachmad Setiawan <rsetiawan7@gmail.com>
2020-03-31 11:59:23 +0700

    Added ApplyDialogComponent

---
+++ src/app/shared/components/dialogs/apply-dialog/services/apply-dialog-factory.service.ts
@@ -0,0 +1,30 @@
+import { TemplateRef, Injectable } from '@angular/core';
+import { MatDialogRef, MatDialog } from '@angular/material';
+import { Observable } from 'rxjs';
+import { first } from 'rxjs/operators';
+
+// Components
+import { ApplyDialogComponent } from '../apply-dialog.component';
+import { ApplyDialogService } from './apply-dialog.service';
+import { ApplyDialog } from '../models/apply-dialog.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ApplyDialogFactoryService<T = undefined> {
+    constructor(private dialog: MatDialog) {}
+
+    open(dialogData: ApplyDialog, options: any): ApplyDialogService<T> {
+        const dialogRef = this.dialog.open<ApplyDialogComponent<T>, any>(
+            ApplyDialogComponent,
+            {
+                ...options,
+                data: dialogData
+            }
+        );
+
+        dialogRef.afterClosed().pipe(first());
+
+        return new ApplyDialogService<T>(dialogRef);
+    }
+}
