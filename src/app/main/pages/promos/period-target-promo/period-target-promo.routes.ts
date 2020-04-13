9223b516b0d673394cf5486adccabf774f38708e
Rachmad Setiawan <rsetiawan7@gmail.com>
2020-04-13 13:33:20 +0700

    Added route to detail page for Period Target Promo

--- src/app/main/pages/promos/period-target-promo/period-target-promo.routes.ts
+++ src/app/main/pages/promos/period-target-promo/period-target-promo.routes.ts
@@ -13,6 +13,7 @@ import { AuthGuard } from '../../core/auth/auth.guard';
 // The component.
 import { PeriodTargetPromoComponent } from './period-target-promo.component';
 import { PeriodTargetPromoFormComponent } from './pages/form/form.component';
+import { PeriodTargetPromoDetailComponent } from './pages/detail/detail.component';
 // import { SkuAssignmentDetailComponent } from './sku-assignment-detail';
 
 // Routes
@@ -61,6 +62,29 @@ const routes: Routes = [
         //     status: CatalogueStatusResolver
         // },
     },
+    {
+        path: 'view/:id',
+        component: PeriodTargetPromoDetailComponent,
+        canActivate: [AuthGuard, NgxPermissionsGuard],
+        data: {
+            permissions: {
+                only: [
+                    'SUPER_SUPPLIER_ADMIN',
+                    'HEAD_OF_SALES',
+                    'BOS',
+                    'COUNTRY_MANAGER',
+                    'SUPPLIER_ADMIN'
+                ],
+                redirectTo: {
+                    navigationCommands: ['/pages/errors/403'],
+                    navigationExtras: {
+                        replaceUrl: true,
+                        skipLocationChange: true
+                    }
+                }
+            }
+        }
+    }
     // { path: ':id/edit', component: SkuAssignmentFormComponent },
     // { path: ':id/detail', component: SkuAssignmentDetailComponent }
 ];
