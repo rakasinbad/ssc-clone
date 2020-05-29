9e9ab43e44bfc42ccdcf5d465973e93d7a9abd29
Aulia Rahman <aulia@sinbad.co.id>
2020-05-29 13:37:07 +0700

    - Add status pending at OMS - Remove validation phone at Store Form

--- src/app/main/pages/orders/store/selectors/order.selectors.ts
+++ src/app/main/pages/orders/store/selectors/order.selectors.ts
@@ -14,11 +14,11 @@ export const getOrderEntities = createSelector(getOrderState, fromOrder.selectOr
 
 export const getTotalOrderEntity = createSelector(getOrderState, fromOrder.selectOrderTotal);
 
-export const getTotalOrder = createSelector(getOrderState, state => state.orders.total);
+export const getTotalOrder = createSelector(getOrderState, (state) => state.orders.total);
 
 export const getSelectedOrderId = createSelector(
     getOrderState,
-    state => state.orders.selectedOrderId
+    (state) => state.orders.selectedOrderId
 );
 
 export const getSelectedOrder = createSelector(
@@ -31,32 +31,39 @@ export const getSelectedOrder = createSelector(
 // Status Order State
 // -----------------------------------------------------------------------------------------------------
 
-export const getTotalStatus = createSelector(getOrderState, state => state.orders.totalStatus);
+export const getTotalStatus = createSelector(getOrderState, (state) => state.orders.totalStatus);
 
-export const getTotalAllOrder = createSelector(getTotalStatus, state => state.totalOrder);
-export const getTotalNewOrder = createSelector(getTotalStatus, state => state.totalNewOrder);
-export const getTotalPackedOrder = createSelector(getTotalStatus, state => state.totalPackedOrder);
+export const getTotalAllOrder = createSelector(getTotalStatus, (state) => state.totalOrder);
+export const getTotalNewOrder = createSelector(getTotalStatus, (state) => state.totalNewOrder);
+export const getTotalPackedOrder = createSelector(
+    getTotalStatus,
+    (state) => state.totalPackedOrder
+);
 export const getTotalShippedOrder = createSelector(
     getTotalStatus,
-    state => state.totalShippedOrder
+    (state) => state.totalShippedOrder
 );
 export const getTotalDeliveredOrder = createSelector(
     getTotalStatus,
-    state => state.totalDeliveredOrder
+    (state) => state.totalDeliveredOrder
 );
 export const getTotalCompletedOrder = createSelector(
     getTotalStatus,
-    state => state.totalCompletedOrder
+    (state) => state.totalCompletedOrder
+);
+export const getTotalPendingOrder = createSelector(
+    getTotalStatus,
+    (state) => state.totalPendingOrder
 );
 export const getTotalCanceledOrder = createSelector(
     getTotalStatus,
-    state => state.totalCanceledOrder
+    (state) => state.totalCanceledOrder
 );
 
 // -----------------------------------------------------------------------------------------------------
 // Helper State
 // -----------------------------------------------------------------------------------------------------
 
-export const getIsRefresh = createSelector(getOrderState, state => state.isRefresh);
+export const getIsRefresh = createSelector(getOrderState, (state) => state.isRefresh);
 
-export const getIsLoading = createSelector(getOrderState, state => state.isLoading);
+export const getIsLoading = createSelector(getOrderState, (state) => state.isLoading);
