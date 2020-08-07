86382bf77c024bf06acf117e171a42a96df3201d
Rachmad Setiawan <iwan@sinbad.co.id>
2020-08-07 10:23:13 +0700

    feat(portfolio): added Warehouse API Service

--- src/app/main/pages/sales-forces/portfolios/services/index.ts
+++ src/app/main/pages/sales-forces/portfolios/services/index.ts
@@ -1,4 +1,5 @@
 import { PortfoliosApiService } from './portfolios-api.service';
 import { StoresApiService } from './stores-api.service';
+import { WarehousesApiService } from './warehouses-api.service';
 
-export { PortfoliosApiService, StoresApiService };
+export { PortfoliosApiService, StoresApiService, WarehousesApiService };
