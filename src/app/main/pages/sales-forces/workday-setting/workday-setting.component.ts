ea87fbd3a1687d2d2f3ea2efc36958fa89b7a880
bismark <bismark.john.anes@gmail.com>
2020-08-18 18:13:00 +0700

    set value host microsite from env

---
+++ src/app/main/pages/sales-forces/workday-setting/workday-setting.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Store } from '@ngrx/store';
+import { StorageMap } from '@ngx-pwa/local-storage';
+import { IBreadcrumbs } from 'app/shared/models/global.model';
+import { UiActions } from 'app/shared/store/actions';
+import * as fromRoot from 'app/store/app.reducer';
+import { environment } from 'environments/environment';
+
+@Component({
+    selector: 'app-workday-setting',
+    templateUrl: './workday-setting.component.html',
+    styleUrls: ['./workday-setting.component.scss'],
+})
+export class WorkdaySettingComponent implements OnInit {
+    url: SafeResourceUrl;
+    isLoading = true;
+
+    private readonly breadcrumbs: IBreadcrumbs[] = [
+        {
+            title: 'Home',
+        },
+        {
+            title: 'Sales Management',
+        },
+        {
+            title: 'Workday Setting',
+        },
+    ];
+
+    constructor(
+        private domSanitizer: DomSanitizer,
+        private store: Store<fromRoot.State>,
+        private storage: StorageMap
+    ) {}
+
+    ngOnInit(): void {
+        this.store.dispatch(UiActions.createBreadcrumb({ payload: this.breadcrumbs }));
+
+        this.storage.get('user').subscribe((data: any) => {
+            this.url = this.domSanitizer.bypassSecurityTrustResourceUrl(
+                `${environment.microSiteHost}/salessetting?token=${data.token}`
+            );
+        });
+    }
+
+    onLoad(): void {
+        this.isLoading = false;
+    }
+}
