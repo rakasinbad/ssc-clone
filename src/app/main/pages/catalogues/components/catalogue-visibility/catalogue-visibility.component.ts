20ec9f5c249e3cb6b15e9cb890f612ed9e3db34e
Aulia Rahman <aulia@sinbad.co.id>
2020-10-27 15:34:51 +0700

    Add functional to show visibilty type in catalogue detail

--- src/app/main/pages/catalogues/components/catalogue-visibility/catalogue-visibility.component.ts
+++ src/app/main/pages/catalogues/components/catalogue-visibility/catalogue-visibility.component.ts
@@ -3,10 +3,19 @@ import {
     Component,
     EventEmitter,
     Input,
+    OnChanges,
+    OnDestroy,
     OnInit,
     Output,
+    SimpleChanges,
     ViewEncapsulation,
 } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { RxwebValidators } from '@rxweb/reactive-form-validators';
+import { ErrorMessageService } from 'app/shared/helpers';
+import { BehaviorSubject, combineLatest, Subject } from 'rxjs';
+import { filter, map, takeUntil } from 'rxjs/operators';
+import { CatalogueFacadeService } from '../../services';
 
 type IFormMode = 'add' | 'view' | 'edit';
 
@@ -17,7 +26,10 @@ type IFormMode = 'add' | 'view' | 'edit';
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CatalogueVisibilityComponent implements OnInit {
+export class CatalogueVisibilityComponent implements OnInit, OnChanges, OnDestroy {
+    private trigger$: BehaviorSubject<string> = new BehaviorSubject('');
+    private unSubs$: Subject<any> = new Subject();
+
     catalogueContent: {
         'content-card': boolean;
         'mt-16': boolean;
@@ -33,6 +45,8 @@ export class CatalogueVisibilityComponent implements OnInit {
 
     formModeValue: IFormMode = 'add';
 
+    form: FormGroup;
+
     @Input()
     get formMode(): IFormMode {
         return this.formModeValue;
@@ -46,7 +60,79 @@ export class CatalogueVisibilityComponent implements OnInit {
     @Output()
     formModeChange: EventEmitter<IFormMode> = new EventEmitter<IFormMode>();
 
-    constructor() {}
+    constructor(
+        private fb: FormBuilder,
+        private catalogueFacade: CatalogueFacadeService,
+        private errorMessageService: ErrorMessageService
+    ) {}
 
-    ngOnInit() {}
+    ngOnInit(): void {
+        this.form = this.fb.group({
+            status: [
+                'active',
+                [
+                    RxwebValidators.required({
+                        message: this.errorMessageService.getErrorMessageNonState(
+                            'default',
+                            'required'
+                        ),
+                    }),
+                ],
+            ],
+            isBonus: [
+                false,
+                [
+                    RxwebValidators.required({
+                        message: this.errorMessageService.getErrorMessageNonState(
+                            'default',
+                            'required'
+                        ),
+                    }),
+                ],
+            ],
+        });
+
+        if (this.formMode === 'edit' || this.formMode === 'view') {
+            this._patchForm();
+        }
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes['formMode']) {
+            if (
+                (!changes['formMode'].isFirstChange() &&
+                    changes['formMode'].currentValue === 'edit') ||
+                changes['formMode'].currentValue === 'view'
+            ) {
+                this.trigger$.next('');
+            }
+        }
+    }
+
+    ngOnDestroy(): void {
+        this.unSubs$.next();
+        this.unSubs$.complete();
+    }
+
+    private _patchForm(): void {
+        combineLatest([this.trigger$, this.catalogueFacade.catalogue$])
+            .pipe(
+                map(([_, catalogue]) => ({ catalogue })),
+                filter((item) => !!item),
+                takeUntil(this.unSubs$)
+            )
+            .subscribe(({ catalogue: item }) => {
+                const statusCtrl = this.form.get('status');
+                const isBonusCtrl = this.form.get('isBonus');
+
+                statusCtrl.setValue(item.status);
+                isBonusCtrl.setValue(item.isBonus === true);
+
+                if (this.formMode === 'view') {
+                    statusCtrl.disable({ onlySelf: true });
+                } else if (this.formMode === 'edit') {
+                    statusCtrl.enable({ onlySelf: true });
+                }
+            });
+    }
 }
