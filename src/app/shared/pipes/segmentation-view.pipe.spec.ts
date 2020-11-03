cf277899fc4cc0202dbd23f4f0cc6821952138ad
Aulia Rahman <aulia@sinbad.co.id>
2020-11-03 11:10:20 +0700

    Create "segmentationView" pipe

---
+++ src/app/shared/pipes/segmentation-view.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SegmentationViewPipe } from './segmentation-view.pipe';
+
+describe('SegmentationViewPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SegmentationViewPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
