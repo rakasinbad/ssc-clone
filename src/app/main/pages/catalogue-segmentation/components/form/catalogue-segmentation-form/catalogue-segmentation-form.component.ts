1e80cd7b2bcd79942d8607d1d40c78be511deb88
Aulia Rahman <aulia@sinbad.co.id>
2020-11-03 18:07:56 +0700

    Add to handle functional segmentation & formGroup

--- src/app/main/pages/catalogue-segmentation/components/form/catalogue-segmentation-form/catalogue-segmentation-form.component.ts
+++ src/app/main/pages/catalogue-segmentation/components/form/catalogue-segmentation-form/catalogue-segmentation-form.component.ts
@@ -1,6 +1,21 @@
-import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    Input,
+    OnInit,
+    ViewEncapsulation,
+} from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { fuseAnimations } from '@fuse/animations';
+import {
+    StoreSegmentationChannel,
+    StoreSegmentationCluster,
+    StoreSegmentationGroup,
+} from 'app/main/pages/catalogues/models';
 import { ICardHeaderConfiguration } from 'app/shared/components/card-header/models';
+import { Warehouse } from 'app/shared/components/dropdowns/single-warehouse/models/warehouse.model';
+import { StoreSegmentationType } from 'app/shared/components/dropdowns/store-segmentation-2/models';
+import { Selection } from 'app/shared/components/multiple-selection/models';
 
 @Component({
     selector: 'app-catalogue-segmentation-form',
@@ -21,7 +36,111 @@ export class CatalogueSegmentationFormComponent implements OnInit {
         },
     };
 
+    isSelectAllWarehouse: boolean = false;
+
+    @Input()
+    form: FormGroup;
+
     constructor() {}
 
-    ngOnInit() {}
+    ngOnInit(): void {}
+
+    onStoreChannelSelected(ev: StoreSegmentationChannel[]): void {
+        const chosenStoreChannelCtrl = this.form.get('chosenStoreChannel');
+
+        chosenStoreChannelCtrl.markAsDirty();
+        chosenStoreChannelCtrl.markAsTouched();
+
+        if (!ev.length) {
+            chosenStoreChannelCtrl.setValue(null);
+        } else {
+            const newStoreChannels: Selection[] = ev.map((item) => ({
+                id: item.id,
+                label: item.name,
+                group: 'store-segmentation-channels',
+            }));
+
+            chosenStoreChannelCtrl.setValue(newStoreChannels);
+        }
+    }
+
+    onStoreClusterSelected(ev: StoreSegmentationCluster[]): void {
+        const chosenStoreClusterCtrl = this.form.get('chosenStoreCluster');
+
+        chosenStoreClusterCtrl.markAsDirty();
+        chosenStoreClusterCtrl.markAsTouched();
+
+        if (!ev.length) {
+            chosenStoreClusterCtrl.setValue(null);
+        } else {
+            const newStoreClusters: Selection[] = ev.map((item) => ({
+                id: item.id,
+                label: item.name,
+                group: 'store-segmentation-clusters',
+            }));
+
+            chosenStoreClusterCtrl.setValue(newStoreClusters);
+        }
+    }
+
+    onStoreGroupSelected(ev: StoreSegmentationGroup[]): void {
+        const chosenStoreGroupCtrl = this.form.get('chosenStoreGroup');
+
+        chosenStoreGroupCtrl.markAsDirty();
+        chosenStoreGroupCtrl.markAsTouched();
+
+        if (!ev.length) {
+            chosenStoreGroupCtrl.setValue(null);
+        } else {
+            const newStoreGroups: Selection[] = ev.map((item) => ({
+                id: item.id,
+                label: item.name,
+                group: 'store-segmentation-groups',
+            }));
+
+            chosenStoreGroupCtrl.setValue(newStoreGroups);
+        }
+    }
+
+    onStoreTypeSelected(ev: StoreSegmentationType[]): void {
+        const chosenStoreTypeCtrl = this.form.get('chosenStoreType');
+
+        chosenStoreTypeCtrl.markAsDirty();
+        chosenStoreTypeCtrl.markAsTouched();
+
+        if (!ev.length) {
+            chosenStoreTypeCtrl.setValue(null);
+        } else {
+            const newStoreTypes: Selection[] = ev.map((item) => ({
+                id: item.id,
+                label: item.name,
+                group: 'store-segmentation-types',
+            }));
+
+            chosenStoreTypeCtrl.setValue(newStoreTypes);
+        }
+    }
+
+    onWarehouseSelected(ev: Warehouse[]): void {
+        const chosenWarehouseCtrl = this.form.get('chosenWarehouse');
+
+        chosenWarehouseCtrl.markAsDirty();
+        chosenWarehouseCtrl.markAsTouched();
+
+        if (!ev.length) {
+            chosenWarehouseCtrl.setValue(null);
+        } else {
+            const newWarehouses: Selection[] = ev.map((item) => ({
+                id: item.id,
+                label: item.name,
+                group: 'warehouses',
+            }));
+
+            chosenWarehouseCtrl.setValue(newWarehouses);
+        }
+    }
+
+    onSelectAllWarehouse(ev: boolean): void {
+        // console.log('SELECT_ALL_WH', { ev });
+    }
 }
