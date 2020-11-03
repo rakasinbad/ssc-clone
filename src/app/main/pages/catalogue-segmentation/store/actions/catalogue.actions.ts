656bd14d5fc70922623e08d67dd1615a9af3c29f
Aulia Rahman <aulia@sinbad.co.id>
2020-11-03 18:05:31 +0700

    Create catalogue actions

---
+++ src/app/main/pages/catalogue-segmentation/store/actions/catalogue.actions.ts
@@ -0,0 +1,26 @@
+import { createAction, props } from '@ngrx/store';
+import { ErrorHandler } from 'app/shared/models/global.model';
+import { IQueryParams } from 'app/shared/models/query.model';
+
+enum Actions {
+    FetchCataloguesFailure = '[Catalogue Segmentation] Fetch Catalogues Failure',
+    FetchCataloguesRequest = '[Catalogue Segmentation] Fetch Catalogues Request',
+    FetchCataloguesSuccess = '[Catalogue Segmentation] Fetch Catalogues Success',
+}
+
+export const fetchCataloguesRequest = createAction(
+    Actions.FetchCataloguesRequest,
+    props<{ payload: IQueryParams }>()
+);
+
+export const fetchCataloguesSuccess = createAction(
+    Actions.FetchCataloguesSuccess,
+    props<{ data: any }>()
+);
+
+export const fetchCataloguesFailure = createAction(
+    Actions.FetchCataloguesFailure,
+    props<{ payload: ErrorHandler }>()
+);
+
+export type FailureActions = 'fetchCataloguesFailure';
