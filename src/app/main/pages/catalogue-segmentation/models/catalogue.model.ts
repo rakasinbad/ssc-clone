9272858c13b65ab70e3880f4d4efb87546c69b30
Aulia Rahman <aulia@sinbad.co.id>
2020-11-04 20:59:12 +0700

    Update catalogue model

--- src/app/main/pages/catalogue-segmentation/models/catalogue.model.ts
+++ src/app/main/pages/catalogue-segmentation/models/catalogue.model.ts
@@ -1,4 +1,30 @@
+import { EStatus } from 'app/shared/models/global.model';
+
 export interface CatalogueProps {
     readonly id: NonNullable<string>;
+    externalId: string;
+    isBonus: boolean;
     name: string;
+    sku: string;
+    status: EStatus;
+}
+
+export class Catalogue implements CatalogueProps {
+    readonly id: NonNullable<string>;
+    externalId: string;
+    isBonus: boolean;
+    name: string;
+    sku: string;
+    status: EStatus;
+
+    constructor(data: CatalogueProps) {
+        const { id, externalId, isBonus, name, sku, status } = data;
+
+        this.id = id;
+        this.externalId = externalId && externalId.trim();
+        this.isBonus = isBonus;
+        this.name = name && name.trim();
+        this.sku = sku && sku.trim();
+        this.status = status;
+    }
 }
--- src/app/main/pages/catalogue-segmentation/models/index.ts
+++ src/app/main/pages/catalogue-segmentation/models/index.ts
@@ -1,1 +1,2 @@
 export * from './catalogue-segmentation.model';
+export * from './catalogue.model';
