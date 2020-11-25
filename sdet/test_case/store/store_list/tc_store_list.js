d009a63a40f1b67692bd60aa0fbc20123b01b353
dudiadikarya-sinbad <dudi@sinbad.co.id>
2020-11-25 11:45:39 +0700

    PE-35 Sanity Level Store List Done

--- sdet/test_case/store/store_list/tc_store_list.js
+++ sdet/test_case/store/store_list/tc_store_list.js
@@ -1,7 +1,9 @@
 const testCase = {
 	describe: 'Store List Page',
 	positive: {
-        getStoreList: 'As an Admin, I want to access Store List Page'
+        getStoreList: 'As an Admin, I want to access Store List Page',
+        getStoreDetail: 'As an Admin, I want to access Store Detail Page',
+        getVerifTab: 'As an Admin, I want to filter store list based on Verified Status'
 	},
 	negative: {
 	}
