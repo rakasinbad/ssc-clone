315d04437ad662a79c1ef9f30576f361d126a0b8
DhonyImamSaputra <dhony.imamsaputra@larisin.id>
2020-12-09 20:01:27 +0700

    refactoring and add oms test ssc

--- sdet/element/oms_element.js
+++ sdet/element/oms_element.js
@@ -2,7 +2,15 @@ import {Selector } from 'testcafe';
 
 const omsSection = Selector('.nav-link.ng-star-inserted').withExactText('OMS')
 const omsTitlePage = Selector('.m-0')
+const omsRowTableData = Selector('.orders-table .mat-row')
+const omsPendingTab = Selector('.mat-tab-label:nth-child(2)')
 const omsPendingPaymentTab = Selector('.mat-tab-label:nth-child(3)')
+const omsNewOrderTab = Selector('.mat-tab-label:nth-child(4)')
+const omsPackedTab = Selector('.mat-tab-label:nth-child(5)')
+const omsShippedTab = Selector('.mat-tab-label:nth-child(6)')
+const omsDeliveredTab = Selector('.mat-tab-label:nth-child(7)')
+const omsDoneTab = Selector('.mat-tab-label:nth-child(8)')
+const omsCancelledTab = Selector('.mat-tab-label:nth-child(9)')
 const omsStoreNameRow = Selector('.mat-row:nth-child(2) mat-cell:nth-child(4)')
 const omsOrderValueRow = Selector('.mat-row:nth-child(2) mat-cell:nth-child(5)')
 const omsStatusValueRow = Selector('.mat-row:nth-child(2) mat-cell:nth-child(8)')
@@ -13,7 +21,15 @@ const omsDetailStatuValue = Selector('.mat-subheading-2')
 module.exports = {
     omsSection,
     omsTitlePage,
+    omsRowTableData,
+    omsPendingTab,
     omsPendingPaymentTab,
+    omsNewOrderTab,
+    omsPackedTab,
+    omsShippedTab,
+    omsDeliveredTab,
+    omsDoneTab,
+    omsCancelledTab,
     omsStoreNameRow,
     omsOrderValueRow,
     omsStatusValueRow,
--- sdet/test/oms/oms.e2e.js
+++ sdet/test/oms/oms.e2e.js
@@ -1,4 +1,4 @@
-import page from '../../page/oms_page.js'
+import page from '../../page/oms/oms_page.js'
 import roles from '../../helper/supplier_account.js'
 import element from '../../element/oms_element.js'
 
@@ -20,18 +20,14 @@ fixture(`${tc.testCase.describe}`)
         }
     })
 
-    .page `${process.env.SSC_BASE_URL}/pages/account/stores`
+    .page `${process.env.SSC_BASE_URL}/pages/orders`
 
 test(`@sanity ${tc.testCase.positive.getOMSPage}`, async t => {
-    await page
-        .clickOMSPage()
     await t
         .expect((element.omsTitlePage).exists).ok()
 });
 
 test(`@sanity ${tc.testCase.positive.getOMSDetail}`, async t => {
-    await page
-        .clickOMSPage()
     const storeName = await (element.omsStoreNameRow).innerText; 
     const storeValue = await (element.omsOrderValueRow).innerText;
     const storeStatus = await (element.omsStatusValueRow).innerText;
@@ -48,10 +44,66 @@ test(`@sanity ${tc.testCase.positive.getOMSDetail}`, async t => {
         .expect(storeStatusDetail).eql(storeStatus)
 });
 
+test(`@sanity ${tc.testCase.positive.getPendingTab}`, async t => {
+    await page
+        .clickOrderStatusSection('pending')
+    const storeStatus = await (element.omsStatusValueRow).innerText;
+    await t
+        .expect(storeStatus).eql("-")
+});
+
 test(`@sanity ${tc.testCase.positive.getPendingPaymentTab}`, async t => {
     await page
-        .clickPendingPaymentSection()
+        .clickOrderStatusSection('pending_payment')
     const storeStatus = await (element.omsStatusValueRow).innerText;
     await t
         .expect(storeStatus).eql("Pending Payment")
+});
+
+test(`@sanity ${tc.testCase.positive.getNewOrderTab}`, async t => {
+    await page
+        .clickOrderStatusSection('new_order')
+    const storeStatus = await (element.omsStatusValueRow).innerText;
+    await t
+        .expect(storeStatus).eql("New Order")
+});
+
+test(`@sanity ${tc.testCase.positive.getPackedTab}`, async t => {
+    await page
+        .clickOrderStatusSection('packed')
+    const storeStatus = await (element.omsStatusValueRow).innerText;
+    await t
+        .expect(storeStatus).eql("Packed")
+});
+
+test(`@sanity ${tc.testCase.positive.getShippedTab}`, async t => {
+    await page
+        .clickOrderStatusSection('shipped')
+    const storeStatus = await (element.omsStatusValueRow).innerText;
+    await t
+        .expect(storeStatus).eql("Shipped")
+});
+
+test(`@sanity ${tc.testCase.positive.getDeliveredTab}`, async t => {
+    await page
+        .clickOrderStatusSection('delivered')
+    const storeStatus = await (element.omsStatusValueRow).innerText;
+    await t
+        .expect(storeStatus).eql("Delivered")
+});
+
+test(`@sanity ${tc.testCase.positive.getDoneTab}`, async t => {
+    await page
+        .clickOrderStatusSection('done')
+    const storeStatus = await (element.omsStatusValueRow).innerText;
+    await t
+        .expect(storeStatus).eql("Done")
+});
+
+test(`@sanity ${tc.testCase.positive.getCancelledTab}`, async t => {
+    await page
+        .clickOrderStatusSection('canceled')
+    const storeStatus = await (element.omsStatusValueRow).innerText;
+    await t
+        .expect(storeStatus).eql("Canceled")
 });
