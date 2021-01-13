a5dbf5664c95e4c87be7cdb55e3d4ff3137b8075
Aulia Rahman <aulia@sinbad.co.id>
2021-01-13 18:01:26 +0700

    Add exclusive tab in catalogue list

--- src/app/main/pages/catalogues/store/actions/catalogue.actions.ts
+++ src/app/main/pages/catalogues/store/actions/catalogue.actions.ts
@@ -330,6 +330,7 @@ export const fetchTotalCatalogueStatusSuccess = createAction(
             totalBonus: string;
             totalInactive: string;
             totalRegular: string;
+            totalExclusive: string;
         };
     }>()
 );
