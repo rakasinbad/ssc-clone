e056d0f57efdd15ebe5be92f9195ba89e60c16a9
Rizqullah <eryz.taufan@sinbad.co.id>
2021-02-02 14:33:51 +0700

    #EC2-569 Interceptor - add mass upload endpoint

--- src/app/shared/interceptors/http-config.interceptor.ts
+++ src/app/shared/interceptors/http-config.interceptor.ts
@@ -40,7 +40,8 @@ export class HttpConfigInterceptor implements HttpInterceptor {
                 !req.url.endsWith('import-supplier-stores') &&
                 !req.url.endsWith('import-journey-plans') &&
                 !req.url.endsWith('import-sales') &&
-                !req.url.endsWith('import-portfolios')
+                !req.url.endsWith('import-portfolios') &&
+                !req.url.endsWith('mass-upload')
             ) {
                 req = req.clone({
                     headers: req.headers.set('Content-Type', 'application/json')
