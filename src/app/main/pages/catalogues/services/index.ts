b4ac8266a890b9c485f1e6506a82457271eea6d7
Aulia Rahman <aulia@sinbad.co.id>
2021-02-19 16:21:24 +0700

    Functional to get sub brand list

--- src/app/main/pages/catalogues/services/index.ts
+++ src/app/main/pages/catalogues/services/index.ts
@@ -3,3 +3,4 @@ export * from './brand.service';
 export * from './catalogue-facade.service';
 export * from './catalogue-price-segmentation-api.service';
 export * from './catalogues.service';
+export * from './sub-brand-api.service';
