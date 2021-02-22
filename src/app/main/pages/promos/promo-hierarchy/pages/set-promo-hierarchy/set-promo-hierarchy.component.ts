2a7aad45078811b458ba77215386a77e818efd59
Mutiara AR <Marizky2008@gmail.com>
2021-02-22 16:49:23 +0700

    update set promo hierarchy

--- src/app/main/pages/promos/promo-hierarchy/pages/set-promo-hierarchy/set-promo-hierarchy.component.ts
+++ src/app/main/pages/promos/promo-hierarchy/pages/set-promo-hierarchy/set-promo-hierarchy.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
+import { FormGroup, FormArray, FormBuilder, FormControl, } from '@angular/forms';
+import { ErrorMessageService, HelperService, NoticeService } from 'app/shared/helpers';
 
 @Component({
   selector: 'app-set-promo-hierarchy',
@@ -8,13 +10,31 @@ import { MAT_DIALOG_DATA } from '@angular/material';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SetPromoHierarchyComponent implements OnInit {
-    constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+    form: FormGroup;
+    selectLayer: any;
+        constructor(
+            @Inject(MAT_DIALOG_DATA) public data: any,  
+            private fb: FormBuilder,
+            private _$errorMessage: ErrorMessageService,
+        ) {}
   
     ngOnInit(): void {
+        this.form = this.fb.group({
+            id: null,
+            supplierId: null,
+            name: this.data.name,
+            layer: this.data.layer,
+            group: this.data.group
+        });
+        this.selectLayer = this.data.layer;
         // console.log('isi data set promo->', this.data)
         // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
         // Add 'implements OnInit' to the class.
         // this.item = this.data;
+    }
+
+    onSubmit(): void {
+
     }
 
 }
