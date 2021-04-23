bc1e178fa58d7924282bee140c50c2330ec337a6
bismarkanes <bismark.john.anes@gmail.com>
2021-04-23 13:38:02 +0700

    fixed sales management workday setting menu

--- src/app/main/pages/sales-forces/workday-setting/workday-setting.component.ts
+++ src/app/main/pages/sales-forces/workday-setting/workday-setting.component.ts
@@ -6,6 +6,7 @@ import { IBreadcrumbs } from 'app/shared/models/global.model';
 import { UiActions } from 'app/shared/store/actions';
 import * as fromRoot from 'app/store/app.reducer';
 import { environment } from 'environments/environment';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
     selector: 'app-workday-setting',
@@ -13,8 +14,8 @@ import { environment } from 'environments/environment';
     styleUrls: ['./workday-setting.component.scss'],
 })
 export class WorkdaySettingComponent implements OnInit {
-    url: SafeResourceUrl;
-    isLoading = true;
+    url$: BehaviorSubject<SafeResourceUrl> = new BehaviorSubject<SafeResourceUrl>('');
+    isLoading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
 
     private readonly breadcrumbs: IBreadcrumbs[] = [
         {
@@ -38,13 +39,15 @@ export class WorkdaySettingComponent implements OnInit {
         this.store.dispatch(UiActions.createBreadcrumb({ payload: this.breadcrumbs }));
 
         this.storage.get('user').subscribe((data: any) => {
-            this.url = this.domSanitizer.bypassSecurityTrustResourceUrl(
+            const safeUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(
                 `${environment.microSiteHost}/salessetting?token=${data.token}`
             );
+
+            this.url$.next(safeUrl);
         });
     }
 
     onLoad(): void {
-        this.isLoading = false;
+        this.isLoading$.next(false);
     }
 }
