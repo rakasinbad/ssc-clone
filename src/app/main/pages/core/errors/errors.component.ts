1470bc0dc3eeb93945cc14bbd281324178ce6302
Febryan Dion Wartono <febryandionwartono@gmail.com>
2021-08-24 17:50:47 +0700

    Fix Default firebase

--- src/app/main/pages/core/errors/errors.component.ts
+++ src/app/main/pages/core/errors/errors.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
     selector: 'app-errors',
@@ -8,7 +9,17 @@ import { Router } from '@angular/router';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ErrorsComponent implements OnInit {
-    constructor(private router: Router) {}
+    constructor(
+        private router: Router,
+        private angularFireDatabase : AngularFireDatabase,
+    ) {
+        // CHECK MAINTENANCE
+        this.angularFireDatabase.object('/maintenance').valueChanges().subscribe((res:any) => {
+            if(res.ssc) {
+                this.router.navigate(['/pages/errors/maintenance'],{skipLocationChange:true});
+            }
+        });
+    }
 
     ngOnInit(): void {
         // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
--- src/app/main/pages/core/errors/errors.module.ts
+++ src/app/main/pages/core/errors/errors.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { FuseSharedModule } from '@fuse/shared.module';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../../../../../environments/environment';
 
 import { ErrorsRoutingModule } from './errors-routing.module';
 import { ErrorsComponent } from './errors.component';
 
 @NgModule({
     declarations: [ErrorsComponent],
-    imports: [ErrorsRoutingModule, FuseSharedModule]
+    imports: [
+        ErrorsRoutingModule, 
+        FuseSharedModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireDatabaseModule
+    ]
 })
 export class ErrorsModule {}
