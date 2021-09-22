aab805b018413a3b3c94a8465d72dd5e8fdf6099
memorvin <alvina.tjia@yahoo.com>
2021-09-22 13:08:53 +0700

    change order cancel reason source

--- src/app/shared/pipes/order-log.pipe.ts
+++ src/app/shared/pipes/order-log.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'orderLog' })
 export class OrderLogPipe implements PipeTransform {
-    transform(log: any): string {
+    transform(log: any, reason: string): string {
         const updatedByStatuses = ['shipping', 'delivered', 'done'];
 
         if (updatedByStatuses.includes(log.status)) {
@@ -10,7 +10,7 @@ export class OrderLogPipe implements PipeTransform {
         }
 
         if (log.status === 'cancel') {
-            return log.updatedBy ? `${log.description} [updated by ${log.updatedBy}] [reason: ${log.reason || '-'}]` : `${log.description} [reason: ${log.reason || '-'}]`;
+            return log.updatedBy ? `${log.description} [updated by ${log.updatedBy}] [reason: ${reason || '-'}]` : `${log.description} [reason: ${reason || '-'}]`;
         }
 
         return log.updatedBy ? `${log.description} by ${log.updatedBy}` : log.description;
