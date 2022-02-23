1d69f1511487b5b6663988fb44014e0da76d8f42
nadans <nisrinaseptiana1@gmail.com>
2022-02-23 13:20:44 +0700

    change request approval billing id to billing payment id

--- src/app/main/pages/finances/collection/store/effects/reject-approve.effects.ts
+++ src/app/main/pages/finances/collection/store/effects/reject-approve.effects.ts
@@ -306,7 +306,7 @@ export class RejectApproveEffects {
                 ofType(RejectReasonActions.updateBillingPaymentApprovalRequest),
                 withLatestFrom(this.store.select(fromRoot.getRouterState), (action, router) => {
                     return {
-                        id: router.state.params.id,
+                        id: action.payload.id,
                         body: action.payload,
                     };
                 }),
