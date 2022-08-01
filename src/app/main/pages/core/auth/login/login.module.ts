3ec4138fdfdb98db05448476eda10719439551b6
Reza Raka Nugraha <rezaraka00@gmail.com>
2022-08-01 17:15:02 +0700

    fix asset not loaded from scss

--- src/app/main/pages/core/auth/login/login.module.ts
+++ src/app/main/pages/core/auth/login/login.module.ts
@@ -4,9 +4,10 @@ import { MaterialModule } from 'app/shared/material.module';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { SingleSpaModule } from 'single-spa/single-spa.module';
 
 @NgModule({
     declarations: [LoginComponent],
-    imports: [LoginRoutingModule, MaterialModule, FuseSharedModule]
+    imports: [LoginRoutingModule, MaterialModule, FuseSharedModule, SingleSpaModule]
 })
 export class LoginModule {}
