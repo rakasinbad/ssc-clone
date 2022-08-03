1bef5dc9f7e0d1c5ef7549f2bbf3d90c561fdd54
Reza Raka Nugraha <rezaraka00@gmail.com>
2022-08-03 18:43:45 +0700

    sync with real repo

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,8 +1,9 @@
-const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').default;
+const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').default
 
-module.exports = (config, options) => {
-  const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
-  singleSpaWebpackConfig.entry.main = [...new Set(singleSpaWebpackConfig.entry.main)];
+module.exports = (angularWebpackConfig, options) => {
+  const singleSpaWebpackConfig = singleSpaAngularWebpack(angularWebpackConfig, options)
+  singleSpaWebpackConfig.entry.main = [...new Set(singleSpaWebpackConfig.entry.main)]
+
   // Feel free to modify this webpack config however you'd like to
-  return singleSpaWebpackConfig;
-};
+  return singleSpaWebpackConfig
+}
